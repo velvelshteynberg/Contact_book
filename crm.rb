ce1e541a596fc4cf5380e84875c8bcd8d7925fa0
Velvelshteynberg <Velvelshteynberg@outlook.com>
2021-01-17 16:27:05 -0500

    question completed besides for one part

--- crm.rb
+++ crm.rb
@@ -19,13 +19,7 @@ class CRM
   attr_accessor :note
   attr_accessor :all_contacts
 
-  def main_menu
-    p print_main_menu 
-    user_selection = gets.chomp.to_i
-    call_option(user_selection)
-  end
-
-  def print_main_menu
+   def print_main_menu
       puts '[1] Add a new contact'
       puts '[2] Modify an existing contact'
       puts '[3] Delete a contact'
@@ -33,25 +27,7 @@ class CRM
       puts '[5] Search by attribute'
       puts '[6] Exit'
      'Enter a number: '
-  end
-
-  
-
-  def call_option(user_selection)
-    if user_selection == "1"
-    p add_new_contact
-    elsif user_selection == "2"
-    p  modify_existing_contact
-    elsif user_selection == "3"
-      delete_contact
-    elsif user_selection == "4"
-    p  display_all_contacts
-    elsif user_selection == "5"
-      search_by_attribute
-    elsif user_selection == "6"
-      abort "You have chosen to exit"
     end
-  end
 
   def add_new_contact
     print 'Enter First Name: '
@@ -66,14 +42,16 @@ class CRM
     print 'Enter a Note: '
     note = gets.chomp
   
-    Contact.create(first_name, last_name, email, note)
+    new_contact = CRM.new(first_name, last_name, email, note)
+    @@all_contacts << new_contact
+    return new_contact
   end
 
   
   def modify_existing_contact
-    print "What value would you like to modify?"
+    print "What value would you like to modify(first_name, last_name, email, note)?"
     user_value_selection = gets.chomp
-    print "What would you like to change the #{user_value_selection} tp?"
+    print "What would you like to change the #{user_value_selection} to?"
     changed_value = gets.chomp
     if user_value_selection == "first_name"
       @first_name = changed_value
@@ -84,10 +62,13 @@ class CRM
     elsif user_value_selection == "note"
       @note = changed_value
     end 
+    return self 
   end
 
   def delete_contact
-    self.delete
+    @@all_contacts.delete(self)
+    p "you deleted #{self}"
+    "your remaining contacts are #{@@all_contacts}"
   end
 
   def display_all_contacts
@@ -95,14 +76,41 @@ class CRM
   end
 
   def search_by_attribute
-    
+    p "What attribute are you looking for? (first_name, last_name, email, note)?"
+    selected_attribute = gets.chomp 
+    p "What is the corresponding value to this attribute? (e.g. what is the guys first name?)"
+    its_value = gets.chomp
+    @@all_contacts.each do |individual_contact|
+      if @selected_attribute == its_value
+        return individual_contact
+      end  
+      #I dont really know why this is returning all of the value in the array
+    end 
+  end 
+  
+
+  def call_option(user_selection)
+    case user_selection
+    when 1 then add_new_contact
+    when 2 then modify_existing_contact
+    when 3 then delete_contact
+    when 4 then display_all_contacts
+    when 5 then search_by_attribute
+    when 6 then "You have chosen to exit"
+    end
   end
 
+  def main_menu
+    p print_main_menu 
+    user_selection = gets.chomp.to_i
+    call_option(user_selection)
+  end
 
-end
+end 
+
 
 velvel = CRM.new("Velvel", "Shteynberg", "Velvelshteynberg@vs.com", "Heeeeeyyyyyy")
 devorah = CRM.new("Devorah", "Shteynberg", "Devorahshteynberg@ds.com", "Helllllooooooo")
 avremel = CRM.new("Avremel", "Shteynberg", "Avremelshteynberg@as.com", "babbbbbyyyyyy")
 
-p velvel.main_menu
+p devorah.main_menu
