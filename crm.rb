2ce18b30d7ff2db1c749265578af42d9b91569af
Velvelshteynberg <Velvelshteynberg@outlook.com>
2021-02-09 21:11:41 -0500

    Fully functional contact book app

--- crm.rb
+++ crm.rb
@@ -62,12 +62,22 @@ end
     puts "what is the id value of the contact that you would like to update?"
     id = gets.chomp.to_i
     contact_wanting_to_modify = Contact.find(id)
-    puts "What value would you like to modify in #{contact_wanting_to_modify}(first_name, last_name, email, note)?"
-    user_value_selection = gets.chomp
-    puts "What would you like to change the #{user_value_selection} to?"
+    puts "What value would you like to modify in #{contact_wanting_to_modify.inspect}(first_name, last_name, email, note)?"
+    puts '[1] First name'
+    puts '[2] Last name'
+    puts '[3] Email'
+    puts '[4] Note'
+   'Enter a number: '
+    user_value_selection = gets.chomp.to_i
+    case user_value_selection
+    when 1 then key_value = "first_name"
+    when 2 then key_value = "last_name"
+    when 3 then key_value = "email"
+    when 4 then key_value = "note"
+    end
+    puts "What would you like to change the #{key_value} to?"
     changed_value = gets.chomp
-    contact_wanting_to_modify.update(user_value_selection, changed_value)
-    contact_wanting_to_modify.save
+    contact_wanting_to_modify.update(key_value => changed_value)
   end
 
   def delete_contact
@@ -88,10 +98,21 @@ end
 
   def search_by_attribute
     p "What attribute are you looking for? (first_name, last_name, email, note)?"
-    selected_attribute = gets.chomp 
+    puts '[1] First name'
+    puts '[2] Last name'
+    puts '[3] Email'
+    puts '[4] Note'
+   'Enter a number: '
+    selected_attribute = gets.chomp.to_i 
+    case selected_attribute
+    when 1 then key_value = "first_name"
+    when 2 then key_value = "last_name"
+    when 3 then key_value = "email"
+    when 4 then key_value = "note"
+    end
     p "What is the corresponding value to this attribute? (e.g. what is the guys first name?)"
     its_value = gets.chomp 
-   "The contact that you are looking for is #{Contact.find_by(selected_attribute, its_value)}"
+    puts "The contact that you are looking for is #{Contact.find_by(key_value => its_value).inspect}"
   end 
   
 
